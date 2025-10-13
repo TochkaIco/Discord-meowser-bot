a8e8733cef767464ac07984e81245657552619f2
tochka@archlinux <25fero@stockholmscience.se>
2025-10-13 11:45:08 +0200

    added slash commands

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 import os
+import random
 from random import randint
 from dotenv import load_dotenv
 
@@ -7,6 +10,15 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 arch_site = """Just go and install Arch ISO already :<
 \nhttps://archlinux.org/"""
+responses = [
+    f"{interaction.user.mention} bites {target.mention}",
+    f"{interaction.user.mention} chomps on {target.mention}",
+]
+gif_bites = [
+    "https://tenor.com/view/chomp-bite-arm-gif-7083692912057941766",
+    "https://tenor.com/view/mikisi-kisi-kiss-gif-27218966",
+    "https://tenor.com/view/slow-cat-bite-cat-bite-slow-gif-26064423",
+]
 
 def random_meow():
     meow_list = ["meow-mror-mrppppp :333333", "meow~", "-# mwow~", "meowieeeeeeeeeeeee :>", "mreowiehehe >:3"]
@@ -17,15 +29,15 @@ def random_meow():
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}!')
+    print(f'Logged in as {bot.user}!')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if ("cat" in message.content.lower()) or ("furry" in message.content.lower()):
@@ -33,4 +45,14 @@ async def on_message(message):
     if "arch" in message.content.lower():
         await message.channel.send(arch_site)
 
-client.run(TOKEN)
+@bot.tree.command(name="bite", description="Bite someone!")
+@app_commands.describe(target="The user you want to bite")
+async def bite(interaction: discord.Interaction, target: discord.User):
+    if target == interaction.user:
+        await interaction.response.send_message("You can’t bite yourself! 🫢", ephemeral=True)
+        return
+    embed = discord.Embed(description=random.choice(responses), color=discord.Color.red())
+    embed.set_image(random.choice(gif_bites))
+    await interaction.response.send_message(embed=embed)
+
+bot.run(TOKEN)
